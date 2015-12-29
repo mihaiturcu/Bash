d11d2eb1ec19f8e51a1f3ccfad5b51156c64f8d6
Mihai Turcu <contact@mihaiturcu.ro>
2015-12-29 15:01:11 +0200

    Final fixes

--- universal-backup/backupWP.sh
+++ universal-backup/backupWP.sh
@@ -4,7 +4,7 @@
 logfile="/var/log/wpbackuplog.log"
 filename="bkp_wordpress2_"$(eval date +%Y%m%d)".tar.gz"
 pathToBlog="/home/black/Downloads" # ex: /var/www/wordpress2/ 
-#pathToBKP="/tmp/bkp/" 
+pathToBKP="/tmp/bkp/" 
 daystodelete="14" # how many days to keep logs :)
 check_dir_writable()
 {
@@ -17,85 +17,42 @@ check_dir_writable()
 }
 check_var_set()
 {
-	if [ -z $"(`echo $1`)" ]
-	then
-		echo "$1 variable not set"
-		exit
-	fi
+	for bar in "$@"
+	do
+		if [ -z ${!bar} ]
+		then
+			echo "$bar variable not set" | tee -a $logfile
+			exit
+		fi
+	done
 
 }
 satisfy_prereq()
 {
-	set -x
-	check_var_set logfile
-	check_var_set pathToBlog
-	check_var_set pathToBKP
-	echo "Starting prereq checks"
-	if (check_dir_writable $logfile) && (check_dir_writable $pathToBKP)
+	check_var_set logfile pathToBlog pathToBKP daystodelete
+	if !(check_dir_writable $logfile) && !(check_dir_writable $pathToBKP)
 	then
-		echo "seems ok"
-	else
-		echo "failed prereq"
+		echo "Backup dir/logfile not writable"
 		exit
 	fi
-	set +x
 }
-
 main()
 {
-	
 	cd /tmp || exit #will exit if can't cd into /tmp
 	satisfy_prereq
-# pana aci	
-	if ([[ "$pathToBlog" ]] && [[ "$pathToBKP" ]]) #check if the variable is set to something
+	
+	echo "Started backup @ $(date)" | tee -a $logfile
+	tar -zcvf "$filename" "$pathToBlog"
+	
+	if [ $? -eq 0 ]
 	then
-		if [ -w $pathToBKP ]
-		then
-			echo "$pathToBKP writable"
-		else
-			echo "$pathToBKP not writable"
-			exit
-		fi
-
-		if ([ ! -d "$pathToBKP" ])
-		then
-			mkdir -p $pathToBKP
-			if [ $? -eq 0 ]
-			then
-				if check_dir_writable $logfile
-				then
-					echo "Logfile not writable, check $logfile"
-					exit
-				fi
-				echo "Directory $pathToBKP was created">>$logfile
-			else
-				echo "Could not create $pathToBKP, you can't write there">>$logfile
-				exit
-			fi	
-		else
-			if [ ! -w "$pathToBKP" ]
-			then
-				echo "$pathToBKP directory is not writable by current user"
-				exit
-			fi
-		fi
-		
-		
-		echo "Started backup @ $(date)">>$logfile
-		tar -zcvf "$filename" "$pathToBlog"
-		
-		if [ $? -eq 0 ]
-		then
-			echo "OK, backup finished @ $(date)">>$logfile
-		else
-			echo "FAIL, backup failed @ $(date)">>$logfile
-			exit
-		fi
-		
-		mv "$filename" "$pathToBKP"
-		find $pathToBKP -type f -mtime +$daystodelete -exec rm -f{} \;
+		echo "OK, backup finished @ $(date)" | tee -a $logfile
 	else
-		echo "Some variables are not set, check script"
+		echo "FAIL, backup failed @ $(date)" | tee -a $logfile
+		exit
 	fi
+		
+	mv "$filename" "$pathToBKP"
+	find $pathToBKP -type f -mtime +$daystodelete -exec rm -f{} \;
 }
 main
