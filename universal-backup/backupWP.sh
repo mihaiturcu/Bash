9cc339118b69136d6d234334945acacba8ce97d2
Mihai Turcu <contact@mihaiturcu.ro>
2015-12-29 11:28:24 +0200

    Changed backupWP.sh if checks

--- universal-backup/backupWP.sh
+++ universal-backup/backupWP.sh
@@ -11,27 +11,31 @@ main()
 {
 	
 	cd /tmp || exit #will exit if cant' cd into /tmp
-	if [ ! -d "$pathToBKP" ]
-	then
-		mkdir -p $pathToBKP
-		if [ $? -eq 0 ]
-		then
-			echo "Directory $pathToBKP was created">>$logfile
-		else
-			echo "Could not create $pathToBKP, you can't write there">>$logfile
-			exit
-		fi	
-	fi
 	if [[ "$pathToBlog" ]] && [[ "$pathToBKP" ]] #check if the variable is set to something
 	then
+		
+		if [ ! -d "$pathToBKP" ]
+		then
+			mkdir -p $pathToBKP
+			if [ $? -eq 0 ]
+			then
+				echo "Directory $pathToBKP was created">>$logfile
+			else
+				echo "Could not create $pathToBKP, you can't write there">>$logfile
+				exit
+			fi	
+		fi
+		
 		echo "Started backup @ $(date)">>$logfile
 		tar -zcvf $filename $pathToBlog
+		
 		if [ $? -eq 0 ]
 		then
 			echo "OK, backup finished @ $(date)">>$logfile
 		else
 			echo "FAIL, backup failed @ $(date)">>$logfile
 		fi
+		
 		mv $filename $pathToBKP
 		find $pathToBKP -type f -mtime +$daystodelete -exec rm -f{} \;
 	fi
